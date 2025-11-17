3194f6b58cf7e62a2d077bcb318c479b20b2bd19
Meghana2417 <118287463+Meghana2417@users.noreply.github.com>
2025-11-17 14:27:20 +0530

    Update settings.py

--- PaymentService/settings.py
+++ PaymentService/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t3c-*(j58i&_-li=^s4p1t8wdy3gqo@piv7&vi-!n0n#i==)cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -137,7 +142,8 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "Next.in@123",
-        "HOST": "db.fukqlfzjejxdljayrqmt.supabase.co",
+        #"HOST": "db.fukqlfzjejxdljayrqmt.supabase.co",
+        'HOST': '172.17.0.1',
         "PORT": "5432",
         "OPTIONS" :{
             "options": "-c search_path=payment_service,public"
